b9f015e07ff731973928062b3b8f8b56c3e909cb
jayden <46307433+kermeow@users.noreply.github.com>
2025-04-13 22:26:09 +0100

    feat: browser config

---
+++ config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"os"
+
+	"github.com/BurntSushi/toml"
+)
+
+type Config struct {
+	GameDir    string   `toml:"game_dir"`
+	SearchDirs []string `toml:"search_dirs"`
+}
+
+func Default() *Config {
+	return &Config{
+		GameDir:    "C:\\Program Files (x86)\\FastGH3\\",
+		SearchDirs: make([]string, 0),
+	}
+}
+
+func Load() (*Config, error) {
+	d := Default()
+
+	if _, err := os.Stat("fastlauncher.toml"); os.IsNotExist(err) {
+		return d, nil
+	}
+
+	_, err := toml.DecodeFile("fastlauncher.toml", &d)
+	return d, err
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require gioui.org v0.8.0
 
 require (
 	gioui.org/shader v1.0.8 // indirect
+	github.com/BurntSushi/toml v1.5.0 // indirect
 	github.com/go-text/typesetting v0.2.1 // indirect
 	golang.org/x/exp v0.0.0-20240707233637-46b078467d37 // indirect
 	golang.org/x/exp/shiny v0.0.0-20240707233637-46b078467d37 // indirect
