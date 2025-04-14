c90215922876c3a2829fab98f4e74fffe9b83ecd
jayden <46307433+kermeow@users.noreply.github.com>
2025-04-14 19:34:33 +0100

    feat: fuzzy searching

--- browser/widgets/songlist.go
+++ browser/widgets/songlist.go
@@ -2,8 +2,10 @@ package widgets
 
 import (
 	"fastgh3/fastbrowser/gh"
+	"fmt"
 	"image"
 	"image/color"
+	"strings"
 
 	"gioui.org/io/event"
 	"gioui.org/io/pointer"
@@ -12,7 +14,9 @@ import (
 	"gioui.org/op/clip"
 	"gioui.org/op/paint"
 	"gioui.org/unit"
+	"gioui.org/widget"
 	"gioui.org/widget/material"
+	"github.com/lithammer/fuzzysearch/fuzzy"
 )
 
 type SongList struct {
@@ -20,7 +24,9 @@ type SongList struct {
 	Charts        *[]*gh.Chart
 	SelectedChart *gh.Chart
 
-	scrollPosition float32
+	search         *widget.Editor
+	displayCharts  []*gh.Chart
+	scrollPosition int
 	buttons        []*SongButton
 }
 
@@ -30,12 +36,48 @@ func NewSongList(theme *material.Theme, charts *[]*gh.Chart) *SongList {
 		Charts:        charts,
 		SelectedChart: nil,
 
+		search:         &widget.Editor{SingleLine: true},
+		displayCharts:  make([]*gh.Chart, 0),
 		scrollPosition: 0,
 		buttons:        make([]*SongButton, 0),
 	}
 }
 
 func (sl *SongList) Layout(gtx layout.Context) layout.Dimensions {
+	area := gtx.Constraints.Max
+	defer clip.Rect{Max: area}.Push(gtx.Ops).Pop()
+	paint.Fill(gtx.Ops, color.NRGBA{0, 0, 0, 150})
+
+	if len(*sl.Charts) > 0 {
+		gtx.Constraints = layout.Exact(image.Pt(area.X, 32))
+
+		searchBox := material.Editor(sl.Theme, sl.search, "Search for a song")
+		searchBox.SelectionColor = color.NRGBA{21, 112, 239, 255}
+		inset := layout.UniformInset(unit.Dp(4))
+		inset.Layout(gtx, searchBox.Layout)
+
+		searchTerm := strings.ToLower(strings.TrimSpace(sl.search.Text()))
+		if len(searchTerm) == 0 {
+			sl.displayCharts = (*sl.Charts)[:]
+		} else {
+			sl.displayCharts = make([]*gh.Chart, 0)
+			for _, chart := range *sl.Charts {
+				if !fuzzy.MatchNormalized(searchTerm, strings.ToLower(fmt.Sprintf("%s - %s", chart.Artist, chart.Name))) {
+					continue
+				}
+				sl.displayCharts = append(sl.displayCharts, chart)
+			}
+		}
+
+		defer op.Offset(image.Pt(0, 32)).Push(gtx.Ops).Pop()
+		gtx.Constraints = layout.Exact(area.Sub(image.Pt(0, 32)))
+	}
+	sl.drawCharts(gtx)
+
+	return layout.Dimensions{Size: area}
+}
+
+func (sl *SongList) drawCharts(gtx layout.Context) layout.Dimensions {
 	defer clip.Rect{Max: gtx.Constraints.Max}.Push(gtx.Ops).Pop()
 	event.Op(gtx.Ops, sl)
 
@@ -48,36 +90,35 @@ func (sl *SongList) Layout(gtx layout.Context) layout.Dimensions {
 		if x, ok := ev.(pointer.Event); ok {
 			switch x.Kind {
 			case pointer.Scroll:
-				sl.scrollPosition += x.Scroll.Y
+				sl.scrollPosition += x.Scroll.Round().Y
 			}
 		}
 	}
 
-	paint.Fill(gtx.Ops, color.NRGBA{0, 0, 0, 150})
+	noCharts := len(sl.displayCharts)
+	maxOnScreen := gtx.Constraints.Max.Y / 36
 
 	if sl.scrollPosition < 0 {
 		sl.scrollPosition = 0
 		gtx.Execute(op.InvalidateCmd{})
 	}
-	noCharts := len(*sl.Charts)
-	if sl.scrollPosition > float32(noCharts-1) {
-		sl.scrollPosition = float32(noCharts-1)
+	if sl.scrollPosition > noCharts - maxOnScreen {
+		sl.scrollPosition = noCharts - maxOnScreen
 		gtx.Execute(op.InvalidateCmd{})
 	}
 
 	gtx.Constraints.Max.X -= 8
 	gtx.Constraints.Max.Y -= 8
 	defer op.Offset(image.Pt(4, 4)).Push(gtx.Ops).Pop()
-	// defer op.Offset(image.Pt(0, int(-sl.scrollPosition*36))).Push(gtx.Ops).Pop()
 
 	if noCharts > 0 {
 		i := 0
 		minI := int(sl.scrollPosition)
-		maxI := minI + (gtx.Constraints.Max.Y / 36) + 1
+		maxI := minI + maxOnScreen + 1
 		if maxI > noCharts {
 			maxI = noCharts
 		}
-		for _, chart := range (*sl.Charts)[minI:maxI] {
+		for _, chart := range sl.displayCharts[minI:maxI] {
 			if len(sl.buttons) < i+1 {
 				btn := &SongButton{Chart: chart, Theme: sl.Theme}
 				sl.buttons = append(sl.buttons, btn)
